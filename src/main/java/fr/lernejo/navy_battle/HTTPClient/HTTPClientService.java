d3e1c3689b2c815c2bb26e919df32ddef179f3cb
Louis Bajard <louis.bajard@epita.fr>
2023-01-10 11:10:38 +0100

    Fix ping handler

---
+++ src/main/java/fr/lernejo/navy_battle/HTTPClient/HTTPClientService.java
@@ -0,0 +1,30 @@
+package fr.lernejo.navy_battle.HTTPClient;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HTTPClientService {
+
+    String adversaryUrl;
+
+    public HTTPClientService(String adversaryUrl) {
+        this.adversaryUrl = adversaryUrl;
+
+    }
+
+    public void post(String path, String body) throws IOException, InterruptedException {
+        HttpRequest requetePost = HttpRequest.newBuilder()
+            .uri(URI.create(adversaryUrl + path))
+            .setHeader("Accept", "application/json")
+            .setHeader("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(body))
+            .build();
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpResponse<String> response = client.send(requetePost, HttpResponse.BodyHandlers.ofString());
+
+    }
+
+}
--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,6 +1,7 @@
 package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpServer;
+import fr.lernejo.navy_battle.HTTPClient.HTTPClientService;
 import fr.lernejo.navy_battle.api.handlers.game.ApiHandlerGameFactory;
 import fr.lernejo.navy_battle.api.handlers.ping.ApiHandlerPingFactory;
 
@@ -12,10 +13,11 @@ import java.util.concurrent.Executors;
 
 public class Launcher {
 
-    public static void main(String[] argv) {
 
+    public static void main(String[] args) {
+
+        int port = Integer.parseInt(args[1]);
         HttpServer server;
-        int port = 9876;
         String host = "localhost";
         try {
             server = HttpServer.create(new InetSocketAddress(host, port), 0);
@@ -23,14 +25,25 @@ public class Launcher {
             throw new RuntimeException(e);
         }
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService executor = Executors.newFixedThreadPool(1);
         server.setExecutor(executor);
         server.createContext("/ping", ApiHandlerPingFactory.GetPingHandler());
         server.createContext("/api/game/start", ApiHandlerGameFactory.GetGameStartHandler());
         server.start();
 
-        System.out.println("Server is ready on: http://" + host + ":" + port);
 
+        System.out.println("Server is ready on: http://" + host + ":" + port);
+        if (args.length > 1) {
+            String url = args[2];
+            HTTPClientService clientService = new HTTPClientService(url);
+            try {
+                clientService.post("api/start/game", "{\"id\":\"1\", \"url\":\"http://localhost:" + port + "\", \"message\":\"hello\"}");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 }
